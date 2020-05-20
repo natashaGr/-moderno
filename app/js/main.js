7dacc5d96d39c1d61941548906b54e81c319d9dc
natashaGr <jeanbilli766@gmail.com>
2020-05-20 18:25:51 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
-$(function(){
-    
+
+$(function () {
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        readOnly: true,
+        starWidth: "12px"
+    });
+    $('.product-slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true,
+    });
+
+
+    var mixer = mixitup('.product__inner-box');
+
 });
