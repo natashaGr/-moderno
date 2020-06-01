98596bc2337c3b1b0c07296651600bbea1e60925
natashaGr <jeanbilli766@gmail.com>
2020-06-01 17:10:06 +0300

    added media

--- app/js/main.js
+++ app/js/main.js
@@ -33,6 +33,14 @@ $(function () {
         $('.icon-th-list').removeClass('active');
     });
 
+    $('.menu__btn').on('click', function(){
+        $('.menu__item').slideToggle();
+    });
+
+    $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+    });
+   
 
     var mixer = mixitup('.product__inner-box');
 
