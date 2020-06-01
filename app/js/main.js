659c3c2c8c86a8f6824acec700604cbf75645b9c
natashaGr <jeanbilli766@gmail.com>
2020-06-02 01:42:00 +0300

    added product-single pages

--- app/js/main.js
+++ app/js/main.js
@@ -33,14 +33,22 @@ $(function () {
         $('.icon-th-list').removeClass('active');
     });
 
-    $('.menu__btn').on('click', function(){
+    $('.menu__btn').on('click', function () {
         $('.menu__item').slideToggle();
     });
 
-    $('.header__btn-menu').on('click', function(){
+    $('.header__btn-menu').on('click', function () {
         $('.header__box').toggleClass('active');
     });
-   
+
+    $('.product-one__tabs .tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
 
     var mixer = mixitup('.product__inner-box');
 
